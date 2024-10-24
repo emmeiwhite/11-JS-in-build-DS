409d04cfc97147eb28f4b0847987537668cd36ee
emmeiwhite <emmeiwhite@gmail.com>
2024-10-24 17:26:03 +0530

    Map() DS ---> holds data as key-value pairs !

---
+++ index.js
@@ -0,0 +1,7 @@
+const cart = new Map()
+
+cart.set('product1', { id: 'product1', name: 'samsung', price: 399, quantity: 1 })
+cart.set('product2', { id: 'product2', name: 'iphone', price: 899, quantity: 1 })
+cart.set('product3', { id: 'product3', name: 'vivo', price: 299, quantity: 1 })
+console.log(cart)
+console.log(cart.get('product3'))
